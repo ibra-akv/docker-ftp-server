1c6519eb4297705966727b33767b29bc4d97bf3c
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2022-12-09 10:57:38 +0100

    2 users support

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -13,9 +13,25 @@ adduser \
 	-u $UID \
 	$FTP_USER
 
+if [[ -v "$FTP_USER2" ]]; then
+	adduser \
+		 -D \
+		 -G $FTP_USER2 \
+		 -h /home/$FTP_USER2 \
+		 -s /bin/false \
+		 -u $UID \
+		 $FTP_USER2
+fi
+
 mkdir -p /home/$FTP_USER
 chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
 echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
+
+if [[ -v "$FTP_USER2" ]]; then
+	mkdir -p /home/$FTP_USER2
+	chown -R $FTP_USER2:$FTP_USER2 /home/$FTP_USER2
+	echo "$FTP_USER2:$FTP_PASS2" | /usr/sbin/chpasswd
+fi
 
 touch /var/log/vsftpd.log
 tail -f /var/log/vsftpd.log | tee /dev/stdout &
